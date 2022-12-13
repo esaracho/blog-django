fc8c77531add6e8efe528e69e02a89ed526b6462
chino <sarachoesteban@gmail.com>
2022-12-13 15:27:42 -0300

    Se agregan urls y vistas

--- misitio/settings.py
+++ misitio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$at*u!kk^jexeh#r(uhc&!y@zf#)_3m(($w^3^a=%2llglrnn9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
